1897d1ae8bbbf66b94cce105ac408269c856a0ae
shuihui_huang <shuihui_huang@163.com>
2018-10-24 00:33:53 +0800

    日志类

---
+++ wordpress/src/main/java/com/qa/logout/LogType.java
@@ -0,0 +1,13 @@
+package com.qa.logout;
+
+public class LogType {
+
+    public LogType(){}
+
+    public enum LogTypeName{
+        INFO,
+        ERROR,
+        WARNING,
+        DEBUG;
+    }
+}
